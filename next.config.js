e5df80e982eb7f00ccf8fdd5a9ade85814de9299
Ilia Mishchenko <mishenkoil02@gmail.com>
2024-08-05 13:01:12 +0300

    деплой статического сайта

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
+  basePath: '/shop-form-test',
+  assetPrefix: '/shop-form-test/',
   trailingSlash: true,
 }
 
